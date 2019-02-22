0d0721b1e27000d646d438aa386112e7e7c94ef3
namvay <namvay@bbv.ch>
2019-02-22 12:48:55 +0700

    push

---
+++ precache-manifest.95c461a901b9e1d8996f7717631bae62.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "f003fcf4cd7038d207c0",
+    "url": "./static/css/main.a7494753.chunk.css"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "75dd28b6b76e704f998e",
+    "url": "./static/js/2.75dd28b6.chunk.js"
+  },
+  {
+    "revision": "f003fcf4cd7038d207c0",
+    "url": "./static/js/main.f003fcf4.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "75dd28b6b76e704f998e",
+    "url": "./static/css/2.5b5dc547.chunk.css"
+  },
+  {
+    "revision": "eb22c318d87588fa1ec61b962346dccd",
+    "url": "./index.html"
+  }
+];
